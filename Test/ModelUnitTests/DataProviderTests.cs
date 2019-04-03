435bb9968016be8b3a4842119e0171565548b2d4
bulumutka <mishasdk@gmail.com>
2019-04-03 21:49:06 +0300

    Changed path for TestSource folder

--- Test/ModelUnitTests/DataProviderTests.cs
+++ Test/ModelUnitTests/DataProviderTests.cs
@@ -12,7 +12,7 @@ namespace Testing {
         [TestMethod()]
         public void CreateListFrom_testList_01() {
             var str = "\\testList_01.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             var expected = new List<int>[] {
                 new List<int> {1, 2},
                 new List<int> {2, 3},
@@ -30,7 +30,7 @@ namespace Testing {
         [TestMethod()]
         public void CreateListFrom_testList_02() {
             var str = "\\testList_02.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             var expected = new List<int>[] {
                 new List<int> {1},
                 new List<int> {0, 2},
@@ -47,28 +47,28 @@ namespace Testing {
         [TestMethod()]
         public void CreateListFrom_testList_03() {
             var str = "\\testList_03.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             Assert.ThrowsException<DataProviderException>(() => DataProvider.CreateAdjacencyListFromFile(path));
         }
 
         [TestMethod()]
         public void CreateListFrom_testList_04() {
             var str = "\\testList_04.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             Assert.ThrowsException<DataProviderException>(() => DataProvider.CreateAdjacencyListFromFile(path));
         }
 
         [TestMethod()]
         public void CreateListFrom_testList_05_EmptyFile() {
             var str = "\\testList_05.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             Assert.ThrowsException<DataProviderException>(() => DataProvider.CreateAdjacencyListFromFile(path));
         }
 
         [TestMethod()]
         public void CreateListFrom_testList_06_WrongOrder() {
             var str = "\\testList_06.txt";
-            string path = Resource1.TestSource + str;
+            string path = "../../TestSource/" + str;
             Assert.ThrowsException<DataProviderException>(() => DataProvider.CreateAdjacencyListFromFile(path));
         }
 
