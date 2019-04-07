8a38840ef2e5ed46aaa7f01db6bf353879ce11ee
bulumutka <mishasdk@gmail.com>
2019-04-07 13:02:11 +0300

    Added new fonts

--- spfgraph/View/MainWindow.xaml.cs
+++ spfgraph/View/MainWindow.xaml.cs
@@ -11,16 +11,40 @@ namespace View {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            DrawEllips();
+            //DrawEllips();
         }
 
-        void DrawEllips() {
-            Ellipse e = new Ellipse();
-            e.Width = 100;
-            e.Height = 60;
-            e.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-            canvas.Children.Add(e);
-        }
+        //void DrawEllips() {
+        //    Ellipse e = new Ellipse {
+        //        Width = 150,
+        //        Height = 60,
+        //        Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0))
+        //    };
+
+        //    canvas.Children.Add(e);
+
+        //    Line line = new Line() {
+        //        X1 = 0,
+        //        Y1 = 0,
+        //        X2 = 100,
+        //        Y2 = 200
+        //    };
+        //    line.StrokeThickness = 5;
+
+        //    line.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        //    canvas.Children.Add(line);
+
+        //    Line myLine = new Line();
+        //    myLine.Stroke = Brushes.Black;
+        //    myLine.X1 = 1;
+        //    myLine.X2 = 50;
+        //    myLine.Y1 = 1;
+        //    myLine.Y2 = 50;
+        //    myLine.HorizontalAlignment = HorizontalAlignment.Left;
+        //    myLine.VerticalAlignment = VerticalAlignment.Center;
+        //    myLine.StrokeThickness = 2;
+        //    canvas.Children.Add(myLine);
+        //}
 
 
     }
