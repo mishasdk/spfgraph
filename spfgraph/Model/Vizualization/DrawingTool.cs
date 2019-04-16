55ea0181f678a599c70c12690b819fd29158c04b
NevMem <memlolkek@gmail.com>
2019-04-16 17:16:09 +0300

    Graph representation optimization

--- spfgraph/Model/Vizualization/DrawingTool.cs
+++ spfgraph/Model/Vizualization/DrawingTool.cs
@@ -25,6 +25,34 @@ namespace Model {
             var widthStep = 60;
             var heightStep = 50;
 
+            for (int i = 1; i != domGraph.GraphLayers.Count; ++i) {
+                var curLayerCounter = new SortedDictionary<int, int>();
+                var curLayerEnterCounter = new SortedDictionary<int, int>();
+                var mx = 0;
+                for (var index = 0; index != domGraph.GraphLayers[i - 1].Count; ++index) {
+                    var value = domGraph.GraphLayers[i - 1][index];
+                    for (int j = 0; j != g.AdjacencyList[value].Length; ++j) {
+                        int to = g.AdjacencyList[value][j];
+                        if (!curLayerCounter.ContainsKey(to)) {
+                            curLayerCounter[to] = 0;
+                            curLayerEnterCounter[to] = 0;
+                        }
+                        curLayerCounter[to] += index + 1;
+                        curLayerEnterCounter[to] += 1;
+                        mx = Math.Max(curLayerCounter[to], mx);
+                    }
+                }
+                domGraph.GraphLayers[i].Sort((a, b) => {
+                    double first = (double)curLayerCounter[a] / curLayerEnterCounter[a];
+                    double second = (double)curLayerCounter[b] / curLayerEnterCounter[b];
+                    if (first == second) return 0;
+                    if (first < second) return -1;
+                    return 1;
+                });
+            }
+
+            var indexByName = new SortedDictionary<int, int>();
+
             for (int i = 0; i < domGraph.GraphLayers.Count; i++) {
                 currentHeight += heightStep;
                 int shift = domGraph.GraphLayers[i].Count * widthStep / 2;
@@ -35,6 +63,7 @@ namespace Model {
                     var point = new Point(currentWidth - shift, currentHeight);
                     var node = new Node(vertex, point);
                     nodes.Add(node);
+                    indexByName[domGraph.GraphLayers[i][j]] = nodes.Count - 1;
                     layouts[i].AddVertex(node);
                 }
             }
@@ -43,7 +72,7 @@ namespace Model {
             for (int i = 0; i < domGraph.AdjacencyList.Length; i++)
                 for (int j = 0; j < domGraph.AdjacencyList[i].Length; j++) {
                     int to = domGraph.AdjacencyList[i][j];
-                    edges.Add(new Edge(nodes[i], nodes[to]));
+                    edges.Add(new Edge(nodes[indexByName[i]], nodes[indexByName[to]]));
                 }
 
             foreach (var edge in edges)
@@ -51,7 +80,6 @@ namespace Model {
 
             foreach (var layout in layouts)
                 layout.DrawElement(Canvas);
-
         }
     }
 }
