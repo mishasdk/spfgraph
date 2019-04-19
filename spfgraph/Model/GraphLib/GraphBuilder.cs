9643d3e330fd54ab98db3649bef3a36a8e0abee0
bulumutka <mishasdk@gmail.com>
2019-04-19 14:49:53 +0300

    Deleted useless methods

--- spfgraph/Model/GraphLib/GraphBuilder.cs
+++ spfgraph/Model/GraphLib/GraphBuilder.cs
@@ -8,36 +8,10 @@ using QuickGraph;
 namespace Model {
     public class GraphBuilder {
         Graph graph;
-        BidirectionalGraph<object, IEdge<object>> graphToViz;
-
         public GraphBuilder(Graph graph) {
             this.graph = graph;
-            graphToViz = new BidirectionalGraph<object, IEdge<object>>();
         }
 
-        public BidirectionalGraph<object, IEdge<object>> CeateBidirectionalGraphToViz() {
-            AddVertices();
-            AddEdges();
-            return graphToViz;
-        }
 
-        void AddVertices() {
-            var ver = new List<string>();
-            for (int i = 0; i < graph.AdjacencyList.Length; i++)
-                ver.Add(i.ToString());
-            graphToViz.AddVertexRange(ver);
-        }
-
-        void AddEdges() {
-            var edg = new List<Edge<object>>();
-            for (int i = 0; i < graph.AdjacencyList.Length; i++) {
-                int v = i;
-                for (int j = 0; j < graph.AdjacencyList[i].Length; j++) {
-                    int to = graph.AdjacencyList[i][j];
-                    edg.Add(new Edge<object>(v.ToString(), to.ToString()));
-                }
-            }
-            graphToViz.AddEdgeRange(edg);
-        }
     }
 }
