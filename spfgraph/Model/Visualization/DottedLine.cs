d5eb9eb6f146a76a1488b2166b759edd05120d09
bulumutka <mishasdk@gmail.com>
2019-05-14 16:54:39 +0300

    Added dotted line for layers

---
+++ spfgraph/Model/Visualization/DottedLine.cs
@@ -0,0 +1,31 @@
+﻿namespace spfgraph.Model.Visualization {
+    /// <summary>
+    /// Class, that encapsulates dotted line's
+    /// data.
+    /// </summary>
+    public class DottedLine : Element {
+
+        #region Public Properties
+
+        public int X1 { get; private set; }
+        public int Y1 { get; private set; }
+        public int X2 { get; private set; }
+        public int Y2 { get; private set; }
+        public int Value { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public DottedLine(int x1, int y1, int x2, int y2, int value) {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+            Value = value;
+        }
+
+        #endregion
+
+    }
+}
--- spfgraph/Model/Visualization/GraphVizBuilder.cs
+++ spfgraph/Model/Visualization/GraphVizBuilder.cs
@@ -214,12 +214,15 @@ namespace spfgraph.Model.Visualization {
         protected ObservableCollection<Element> CreateElementsToShow() {
             var nodes = new List<Node>();
             var edges = new List<Edge>();
+            var dottedLines = new List<DottedLine>();
             var indexByName = new SortedDictionary<int, int>();
             var currentHeight = StartHeight;
 
             // Creating nodes of graph
             for (int i = 0; i < dagGraph.GraphLayers.Count; i++) {
                 int shift = dagGraph.GraphLayers[i].Count * WidthStep / 2;
+                var dottedLine = new DottedLine(-30, currentHeight, dagGraph.GetGraphFeatures().Width * 60 + 20, currentHeight, i);
+                dottedLines.Add(dottedLine);
                 for (int j = 0; j < dagGraph.GraphLayers[i].Count; j++) {
                     var currentWidth = StartLeft + WidthStep * j;
                     var value = dagGraph.GraphLayers[i][j];
@@ -248,6 +251,8 @@ namespace spfgraph.Model.Visualization {
 
             // Creating output colection of elementss
             var elementsCollection = new ObservableCollection<Element>();
+            foreach (var i in dottedLines)
+                elementsCollection.Add(i);
             foreach (var i in edges)
                 elementsCollection.Add(i);
             foreach (var node in nodes)
